51a9ccbf3b81abacaa43f0d9e075323e3209a7e2
yesod30 <samu.9301295@live.it>
2023-03-30 18:25:32 +0200

    Added ThemeManager libraries and setup for debugging and testing

---
+++ WoWsShipBuilder.Web/Utility/MudColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using MudBlazor.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WoWsShipBuilder.Web.Utility;
+
+public class MudColorConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return (objectType == typeof(MudColor));
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        // Load the JSON for the Result into a JObject
+        JObject jo = JObject.Load(reader);
+
+        // Read the properties which will be used as constructor parameters
+        string color = (string)jo["Value"];
+
+        // Construct the Result object using the non-default constructor
+        MudColor result = color;
+
+        // (If anything else needs to be populated on the result object, do that here)
+
+        // Return the result
+        return result;
+    }
+
+    public override bool CanWrite
+    {
+        get { return false; }
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
